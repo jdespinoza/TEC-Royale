af1a95d0de031ce506306a928caf4b268a097efa
Josetri <jmiguelh.19@gmail.com>
2017-09-30 17:13:00 -0600

    se actualiza para probar pero da problemas

--- MPI.c
+++ MPI.c
@@ -1,48 +1,40 @@
+
+// Author: Wes Kendall
+// Copyright 2011 www.mpitutorial.com
+// This code is provided freely with the tutorials on mpitutorial.com. Feel
+// free to modify it for your own use. Any distribution of the code must
+// either provide a link to www.mpitutorial.com or keep this header intact.
+//
+// MPI_Send, MPI_Recv example. Communicates the number -1 from process 0
+// to process 1.
+//
+#include <mpi.h>
 #include <stdio.h>
-#include <string.h>
-#include <mpi.h>/* header de las llamadas a MPI */
+#include <stdlib.h>
 
+int main(int argc, char** argv) {
+  // Initialize the MPI environment
+  MPI_Init(NULL, NULL);
+  // Find out rank, size
+  int world_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  int world_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-//main
-main(int argc, char* argv[]) {
-    int         mi_rango;      /* rango del proceso    */
-    int         p;             /* numero de procesos   */
-    int         fuente;        /* rango del que envia  */
-    int         dest;          /* rango del que recibe */
-    int         tag = 0;       /* etiqueta del mensaje */
-    char        mensaje[100];  /* mensaje  */
-    MPI_Status  estado;   /* devuelve estado al recibir*/
+  // We are assuming at least 2 processes for this task
+  if (world_size < 2) {
+    fprintf(stderr, "World size must be greater than 1 for %s\n", argv[0]);
+    MPI_Abort(MPI_COMM_WORLD, 1);
+  }
 
-    /* Comienza las llamadas a MPI */
-    MPI_Init(&argc, &argv);
-
-    /* Averiguamos el rango de nuestro proceso */
-    MPI_Comm_rank(MPI_COMM_WORLD, &mi_rango);
-
-    /* Averiguamos el número de procesos que estan
-     * ejecutando nuestro porgrama
-     */
-    MPI_Comm_size(MPI_COMM_WORLD, &p);
-
-    if (mi_rango != 0) {
-        /* Crea mensaje */
-        sprintf(mensaje, "Saludos del proceso %d!",
-            mi_rango);
-        dest = 0;
-   /* Usa strlen+1 para que la marca /0 se transmita */
-        MPI_Send(mensaje, strlen(mensaje)+1, MPI_CHAR,
-            dest, tag, MPI_COMM_WORLD);
-    } else { /* mi_rango == 0 */
-        for (fuente = 1; fuente < p; fuente++) {
-            MPI_Recv(mensaje, 100, MPI_CHAR, fuente,
-                tag, MPI_COMM_WORLD, &estado);
-            printf("%s\n", mensaje);
-        }
-    }
-
-    /* Termina con MPI. Recordemos que después de
-     * esta llamada no podemos llamar a funciones
-     * MPI, ni siquiera de nuevo a MPI_Init
-     */
-     MPI_Finalize();
-} /* main */
+  int number;
+  if (world_rank == 0) {
+    // If we are rank 0, set the number to -1 and send it to process 1
+    number = -1;
+    MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+  } else if (world_rank == 1) {
+    MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    printf("Process 1 received number %d from process 0\n", number);
+  }
+  MPI_Finalize();
+}/* main */
